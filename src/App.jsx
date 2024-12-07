c443a2fbb0f1d7be5b237bc0e32b161874875fee
Ksenia Melnyk <melnyksenia@gmail.com>
2024-12-07 17:41:47 +0100

    add profile component functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
 
 const App = () => {
-  <>
-    <Profile />
-  </>;
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
 };
 
 export default App;
